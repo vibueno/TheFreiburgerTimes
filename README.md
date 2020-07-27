a6a251a89f4f5039b820e223553c23030b0918a4
vibueno <victor@localhost.localdomain>
2020-07-27 16:33:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# The Freiburger Times
+
+This project was developed within in the frame of the Front End Developer Nanodegree program at Udacity.
+
+## Technologies & Patterns used:
+
+*	Fully responsive (starting at 240px Unihertz Jelly Pro, as fas as I know the smallest Smartphone widely available)
+* Responsive Patterns: Off Canvas, Column Drop
+* Responsive Images: showing crops for certain pictures
+* Responsive Tables: No more tables & Hidden Columns techniques
+
+## Main features:
+* Mobile navigation menu (hamburguer) with no background scrolling
+* Sticky menu on bigger viewports
+* Back to top button with soft scrolling
+
+## Browser compatibility:
+* Chrome 84.0.4147.89
+* Firefox 78.0.1
+* Opera 69.0.3686.57
+* Falkon 3.1.0
+* Konqueror: some of the JavaScript used for the back to top button and for stopping the background scrolling does not seem to work.
+
+## Real Devices tested:
+* Desktop computer + Developer Tools
+* Uniherz Jelly Pro
+
+## To be tested:
+* IE and Edge
+* Other real devices
---
+++ js/functions.js
@@ -0,0 +1,89 @@
+window.onload=function(){
+
+	const body = document.querySelector('body');
+	const header = document.querySelector('.header');
+	const main = document.querySelector('.main');
+	const drawer = document.querySelector('#drawer');
+	const menu = document.querySelector('#menu');
+	const hamburguer = document.querySelector('.hamburguer');
+	const x = document.querySelector('.x');
+	const menu_entries = document.querySelectorAll('.nav__item > a');
+	const backtotop = document.querySelector('#backtotop');
+
+	let headerPaddingRight = header.style.paddingRight === ''?'0':header.style.paddingRight;
+	let mainPaddingRight = main.style.paddingRight === ''?'0':main.style.paddingRight;
+
+	function setBacktotopVisibility(){
+		if (((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight/1.75) && (!drawer.classList.contains('open'))) {
+			backtotop.style.display = 'block';
+		}
+		else{
+			backtotop.style.display = 'none';
+		};
+	}
+
+	function openMenu(){
+
+		const scrollYWidth = window.innerWidth - document.documentElement.clientWidth;
+
+		drawer.classList.add('open');
+		hamburguer.style.display = 'none';
+		x.style.display = 'block';
+
+		/*Deactivating scrolling*/
+		body.style.overflow = 'hidden';
+		body.style.position = 'absolute';
+		header.style.paddingRight = `${scrollYWidth}px`;
+		main.style.paddingRight = `${scrollYWidth}px`;
+
+		setBacktotopVisibility();
+	}
+
+	function closeMenu(){
+		drawer.classList.remove('open');
+		hamburguer.style.display = 'block';
+		x.style.display = 'none';
+
+		/*Reactivating scrolling*/
+		body.style.overflow = 'auto';
+
+		header.style.paddingRight = `${headerPaddingRight}px`;
+		main.style.paddingRight = `${mainPaddingRight}px`;
+
+		setBacktotopVisibility();
+	}
+
+	setBacktotopVisibility();
+
+	menu.addEventListener('click', function(e) {
+
+		if (!this.classList.contains('open')){
+			openMenu();
+		}
+		else{
+			closeMenu();
+		}
+
+		e.stopPropagation();
+	});
+
+	[header, main, x].forEach(item => {
+	  item.addEventListener('click',  function() {
+			closeMenu();
+		});
+	});
+
+	for (const menu_entry of menu_entries) {
+		menu_entry.addEventListener('click', function() {
+			closeMenu();
+		});
+	};
+
+	backtotop.addEventListener('click', function() {
+		scroll(0,0);
+	});
+
+	window.addEventListener('scroll', function(e) {
+		setBacktotopVisibility();
+	});
+}
