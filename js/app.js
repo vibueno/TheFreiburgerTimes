5b8a173a11aa0acd57c16616e25b4579d1a955e8
vibueno <victor@localhost.localdomain>
2020-08-11 23:35:13 +0200

    Replaced single quotes with double quotes

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 window.onload=function(){
 
-	const body = document.querySelector('body');
-	const header = document.querySelector('.header');
-	const main = document.querySelector('.main');
-	const drawer = document.querySelector('#drawer');
-	const menu = document.querySelector('#menu');
-	const hamburguer = document.querySelector('.hamburguer');
-	const x = document.querySelector('.x');
-	const menu_entries = document.querySelectorAll('.nav__item > a');
-	const backtotop = document.querySelector('.backtotop');
+	const body = document.querySelector("body");
+	const header = document.querySelector(".header");
+	const main = document.querySelector(".main");
+	const drawer = document.querySelector("#drawer");
+	const menu = document.querySelector("#menu");
+	const hamburguer = document.querySelector(".hamburguer");
+	const x = document.querySelector(".x");
+	const menu_entries = document.querySelectorAll(".nav__item > a");
+	const backtotop = document.querySelector(".backtotop");
 
-	let headerPaddingRight = header.style.paddingRight === ''?'0':header.style.paddingRight;
-	let mainPaddingRight = main.style.paddingRight === ''?'0':main.style.paddingRight;
+	let headerPaddingRight = header.style.paddingRight === ""?"0":header.style.paddingRight;
+	let mainPaddingRight = main.style.paddingRight === ""?"0":main.style.paddingRight;
 
 	function setBacktotopVisibility(){
-		if (((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight/1.75) && (!drawer.classList.contains('open'))) {
+		if (((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight/1.75) && (!drawer.classList.contains("open"))) {
 			window.setTimeout( function() {
-				backtotop.classList.remove('backtotop__hidden');
-				backtotop.classList.add('backtotop__visible');
+				backtotop.classList.remove("backtotop__hidden");
+				backtotop.classList.add("backtotop__visible");
 			}, 100);
 
 		}
 		else{
 			window.setTimeout( function() {
-				backtotop.classList.remove('backtotop__visible');
-				backtotop.classList.add('backtotop__hidden');
+				backtotop.classList.remove("backtotop__visible");
+				backtotop.classList.add("backtotop__hidden");
 			}, 100);
 		};
 	}
@@ -33,13 +33,13 @@ window.onload=function(){
 
 		const scrollYWidth = window.innerWidth - document.documentElement.clientWidth;
 
-		drawer.classList.add('open');
-		hamburguer.style.display = 'none';
-		x.style.display = 'block';
+		drawer.classList.add("open");
+		hamburguer.style.display = "none";
+		x.style.display = "block";
 
 		/*Deactivating scrolling*/
-		body.style.overflow = 'hidden';
-		body.style.position = 'absolute';
+		body.style.overflow = "hidden";
+		body.style.position = "absolute";
 		header.style.paddingRight = `${scrollYWidth}px`;
 		main.style.paddingRight = `${scrollYWidth}px`;
 
@@ -47,12 +47,12 @@ window.onload=function(){
 	}
 
 	function closeMenu(){
-		drawer.classList.remove('open');
-		hamburguer.style.display = 'block';
-		x.style.display = 'none';
+		drawer.classList.remove("open");
+		hamburguer.style.display = "block";
+		x.style.display = "none";
 
 		/*Reactivating scrolling*/
-		body.style.overflow = 'auto';
+		body.style.overflow = "auto";
 
 		header.style.paddingRight = `${headerPaddingRight}px`;
 		main.style.paddingRight = `${mainPaddingRight}px`;
@@ -62,9 +62,9 @@ window.onload=function(){
 
 	setBacktotopVisibility();
 
-	menu.addEventListener('click', function(e) {
+	menu.addEventListener("click", function(e) {
 
-		if (!this.classList.contains('open')){
+		if (!this.classList.contains("open")){
 			openMenu();
 		}
 		else{
@@ -75,22 +75,22 @@ window.onload=function(){
 	});
 
 	[header, main, x].forEach(item => {
-	  item.addEventListener('click',  function() {
+	  item.addEventListener("click",  function() {
 			closeMenu();
 		});
 	});
 
 	for (const menu_entry of menu_entries) {
-		menu_entry.addEventListener('click', function() {
+		menu_entry.addEventListener("click", function() {
 			closeMenu();
 		});
 	};
 
-	backtotop.addEventListener('click', function() {
+	backtotop.addEventListener("click", function() {
 		scroll(0,0);
 	});
 
-	window.addEventListener('scroll', function(e) {
+	window.addEventListener("scroll", function(e) {
 		setBacktotopVisibility();
 	});
 }
