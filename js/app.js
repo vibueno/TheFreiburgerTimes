4b13ee3cbcfd5f83228197596cc956d1050dd802
vibueno <victor@localhost.localdomain>
2020-08-30 14:07:24 +0200

    Documentation & small bug fixing

--- js/app.js
+++ js/app.js
@@ -4,10 +4,9 @@ window.onload=function(){
 	const header = document.querySelector(".header");
 	const main = document.querySelector(".main");
 	const drawer = document.querySelector("#drawer");
-	const menu = document.querySelector("#menu");
-	const hamburguer = document.querySelector(".hamburguer");
-	const x = document.querySelector(".x");
-	const menu_entries = document.querySelectorAll(".nav__item > a");
+	const hamburgerMenu = document.querySelector("#hamburgerMenu");
+	const xMenu = document.querySelector("#xMenu");
+	const menu_entries = document.querySelectorAll(".nav-item > a");
 	const backtotop = document.querySelector(".backtotop");
 	const backtotopClassVisible = "backtotop-visible";
 	const backtotopClassHidden = "backtotop-hidden";
@@ -15,8 +14,13 @@ window.onload=function(){
 	let headerPaddingRight = header.style.paddingRight === ""?"0":header.style.paddingRight;
 	let mainPaddingRight = main.style.paddingRight === ""?"0":main.style.paddingRight;
 
+	/**
+	 * Sets the visibility of the back to top button
+	 */
+
 	function setBacktotopVisibility(){
-		if (((window.innerHeight + window.pageYOffset) >= (document.body.offsetHeight/1.75)) && (!drawer.classList.contains("open"))) {
+		if (((window.innerHeight + window.pageYOffset) >= (document.body.offsetHeight/1.75)) &&
+			(!drawer.classList.contains("open"))) {
 			window.setTimeout( function() {
 				backtotop.classList.remove(backtotopClassHidden);
 				backtotop.classList.add(backtotopClassVisible);
@@ -31,13 +35,17 @@ window.onload=function(){
 		}
 	}
 
+	/**
+	 * Opens the side menu on smaller viewports
+	 */
+
 	function openMenu(){
 
 		const scrollYWidth = window.innerWidth - document.documentElement.clientWidth;
 
 		drawer.classList.add("open");
-		hamburguer.style.display = "none";
-		x.style.display = "block";
+		hamburgerMenu.style.display = "none";
+		xMenu.style.display = "block";
 
 		/*Deactivating scrolling*/
 		body.style.overflow = "hidden";
@@ -48,10 +56,14 @@ window.onload=function(){
 		setBacktotopVisibility();
 	}
 
+	/**
+	 * Closes the side menu on smaller viewports
+	 */
+
 	function closeMenu(){
 		drawer.classList.remove("open");
-		hamburguer.style.display = "block";
-		x.style.display = "none";
+		hamburgerMenu.style.display = "block";
+		xMenu.style.display = "none";
 
 		/*Reactivating scrolling*/
 		body.style.overflow = "auto";
@@ -62,9 +74,17 @@ window.onload=function(){
 		setBacktotopVisibility();
 	}
 
-	setBacktotopVisibility();
+	/**
+	 *
+	 * Events
+	 *
+	 */
 
-	menu.addEventListener("click", function(e) {
+	/**
+	 * Toggles the side menu
+	 */
+
+	hamburgerMenu.addEventListener("click", function(e) {
 
 		if (!this.classList.contains("open")){
 			openMenu();
@@ -76,11 +96,27 @@ window.onload=function(){
 		e.stopPropagation();
 	});
 
-	[header, main, x].forEach(item => {
+
+	/**
+	 * Side menu should get closed when the user clicks outside it
+	 * We add a click event ĺistener for all the elements which should
+	 * close the menu when clicked
+	 */
+
+	[header, main, xMenu].forEach(item => {
 	  item.addEventListener("click",  function() {
 			closeMenu();
 		});
 	});
+
+	/*
+	 * Side menu should get closed after clicking on a menu entry
+
+	 * This behaviour could also be achieved with event delegation,
+	 * but since we only have a couple of menu entries,
+	 * it does not pay off to complicate the code
+	 * (we are using vanilla JS)
+	 */
 
 	for (const menu_entry of menu_entries) {
 		menu_entry.addEventListener("click", function() {
@@ -88,11 +124,30 @@ window.onload=function(){
 		});
 	}
 
+	/**
+	 * Scroll to page top when back to top button clicked
+	 */
+
 	backtotop.addEventListener("click", function() {
 		scroll(0,0);
 	});
 
+	/**
+	 * On each scroll, wen need to check whether the back to top button
+	 * should be shown or hidden
+	 */
+
 	window.addEventListener("scroll", function() {
 		setBacktotopVisibility();
 	});
+
+
+	/**
+	 *
+	 * Initial calls
+	 *
+	 */
+
+	setBacktotopVisibility();
+
 };
