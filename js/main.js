8770a22a730beddc87996cd2be6c5f7d1cdab792
vibueno <victor@localhost>
2020-10-26 14:55:54 +0100

    Identation between Sublime Text & Github

--- js/main.js
+++ js/main.js
@@ -1,148 +1,148 @@
 window.onload=function(){
 
-	const BODY = document.querySelector('body');
-	const HEADER = document.querySelector('.header');
-	const MAIN = document.querySelector('main');
-	const DRAWER = document.querySelector('#drawer');
-	const HAMBURGER_MENU = document.querySelector('#hamburger-menu');
-	const X_MENU = document.querySelector('#x-menu');
-	const MENU_ENTRIES = document.querySelectorAll('.nav-item > a');
-	const BACK_TO_TOP = document.querySelector('.back-to-top');
-	const BACK_TO_TOP_CLASS_VISIBLE = 'back-to-top-visible';
-	const BACK_TO_TOP_CLASS_HIDDEN = 'back-to-top-hidden';
+  const BODY = document.querySelector('body');
+  const HEADER = document.querySelector('.header');
+  const MAIN = document.querySelector('main');
+  const DRAWER = document.querySelector('#drawer');
+  const HAMBURGER_MENU = document.querySelector('#hamburger-menu');
+  const X_MENU = document.querySelector('#x-menu');
+  const MENU_ENTRIES = document.querySelectorAll('.nav-item > a');
+  const BACK_TO_TOP = document.querySelector('.back-to-top');
+  const BACK_TO_TOP_CLASS_VISIBLE = 'back-to-top-visible';
+  const BACK_TO_TOP_CLASS_HIDDEN = 'back-to-top-hidden';
 
-	let headerPaddingRight = HEADER.style.paddingRight === ''?'0':HEADER.style.paddingRight;
-	let mainPaddingRight = MAIN.style.paddingRight === ''?'0':MAIN.style.paddingRight;
+  let headerPaddingRight = HEADER.style.paddingRight === ''?'0':HEADER.style.paddingRight;
+  let mainPaddingRight = MAIN.style.paddingRight === ''?'0':MAIN.style.paddingRight;
 
-	/**
+  /**
    * @description Sets the visibility of the back to top button
-	 */
-	function setBacktotopVisibility() {
-		if (((window.innerHeight + window.pageYOffset) >= (BODY.offsetHeight/1.75)) &&
-			(!DRAWER.classList.contains('open'))) {
-			window.setTimeout( function() {
-				BACK_TO_TOP.classList.remove(BACK_TO_TOP_CLASS_HIDDEN);
-				BACK_TO_TOP.classList.add(BACK_TO_TOP_CLASS_VISIBLE);
-			}, 100);
+   */
+  function setBacktotopVisibility() {
+    if (((window.innerHeight + window.pageYOffset) >= (BODY.offsetHeight/1.75)) &&
+      (!DRAWER.classList.contains('open'))) {
+      window.setTimeout( function() {
+        BACK_TO_TOP.classList.remove(BACK_TO_TOP_CLASS_HIDDEN);
+        BACK_TO_TOP.classList.add(BACK_TO_TOP_CLASS_VISIBLE);
+      }, 100);
 
-		}
-		else {
-			window.setTimeout( function() {
-				BACK_TO_TOP.classList.remove(BACK_TO_TOP_CLASS_VISIBLE);
-				BACK_TO_TOP.classList.add(BACK_TO_TOP_CLASS_HIDDEN);
-			}, 100);
-		}
-	}
+    }
+    else {
+      window.setTimeout( function() {
+        BACK_TO_TOP.classList.remove(BACK_TO_TOP_CLASS_VISIBLE);
+        BACK_TO_TOP.classList.add(BACK_TO_TOP_CLASS_HIDDEN);
+      }, 100);
+    }
+  }
 
-	/**
-	 * @description Opens the side menu on smaller viewports
-	 */
-	function openMenu() {
+  /**
+   * @description Opens the side menu on smaller viewports
+   */
+  function openMenu() {
 
-		const scrollYWidth = window.innerWidth - document.documentElement.clientWidth;
+    const scrollYWidth = window.innerWidth - document.documentElement.clientWidth;
 
-		DRAWER.classList.add('open');
-		HAMBURGER_MENU.style.display = 'none';
-		X_MENU.style.display = 'block';
+    DRAWER.classList.add('open');
+    HAMBURGER_MENU.style.display = 'none';
+    X_MENU.style.display = 'block';
 
-		/*Deactivating scrolling*/
-		BODY.style.overflow = 'hidden';
-		BODY.style.position = 'absolute';
-		HEADER.style.paddingRight = `${scrollYWidth}px`;
-		MAIN.style.paddingRight = `${scrollYWidth}px`;
+    /*Deactivating scrolling*/
+    BODY.style.overflow = 'hidden';
+    BODY.style.position = 'absolute';
+    HEADER.style.paddingRight = `${scrollYWidth}px`;
+    MAIN.style.paddingRight = `${scrollYWidth}px`;
 
-		setBacktotopVisibility();
-	}
+    setBacktotopVisibility();
+  }
 
-	/**
-	  * @description Closes the side menu on smaller viewports
-	  */
-	function closeMenu() {
-		DRAWER.classList.remove('open');
-		HAMBURGER_MENU.style.display = 'block';
-		X_MENU.style.display = 'none';
+  /**
+    * @description Closes the side menu on smaller viewports
+    */
+  function closeMenu() {
+    DRAWER.classList.remove('open');
+    HAMBURGER_MENU.style.display = 'block';
+    X_MENU.style.display = 'none';
 
-		/*Reactivating scrolling*/
-		BODY.style.overflow = 'auto';
+    /*Reactivating scrolling*/
+    BODY.style.overflow = 'auto';
 
-		HEADER.style.paddingRight = `${headerPaddingRight}px`;
-		MAIN.style.paddingRight = `${mainPaddingRight}px`;
+    HEADER.style.paddingRight = `${headerPaddingRight}px`;
+    MAIN.style.paddingRight = `${mainPaddingRight}px`;
 
-		setBacktotopVisibility();
-	}
+    setBacktotopVisibility();
+  }
 
-	/**
-	 *
-	 * Events
-	 *
-	 */
+  /**
+   *
+   * Events
+   *
+   */
 
-	/**
-	 * @description Toggles the side menu
-	 */
-	HAMBURGER_MENU.addEventListener('click', function(e) {
+  /**
+   * @description Toggles the side menu
+   */
+  HAMBURGER_MENU.addEventListener('click', function(e) {
 
-		if (!this.classList.contains('open')) {
-			openMenu();
-		}
-		else {
-			closeMenu();
-		}
+    if (!this.classList.contains('open')) {
+      openMenu();
+    }
+    else {
+      closeMenu();
+    }
 
-		e.stopPropagation();
-	});
+    e.stopPropagation();
+  });
 
-	/**
+  /**
    * @description Closes the side menu when user clicks outside of it
-	 *
-	 */
+   *
+   */
 
-	 //event ĺistener added for all elements which should close menu when clicked
-	[HEADER, MAIN, X_MENU].forEach(item => {
-	  item.addEventListener('click',  function() {
-			closeMenu();
-		});
-	});
+   //event ĺistener added for all elements which should close menu when clicked
+  [HEADER, MAIN, X_MENU].forEach(item => {
+    item.addEventListener('click',  function() {
+      closeMenu();
+    });
+  });
 
-	/**
+  /**
    * @description Closes side menu after clicking on menu entry
-	 *
-	 */
+   *
+   */
 
-	/* This behaviour could also be achieved with event delegation,
-	but since we only have a couple of menu entries,
-	it does not pay off to complicate the code
-	(we are using vanilla JS) */
+  /* This behaviour could also be achieved with event delegation,
+  but since we only have a couple of menu entries,
+  it does not pay off to complicate the code
+  (we are using vanilla JS) */
 
-	for (const MENU_ENTRY of MENU_ENTRIES) {
-		MENU_ENTRY.addEventListener('click', function() {
-			closeMenu();
-		});
-	}
+  for (const MENU_ENTRY of MENU_ENTRIES) {
+    MENU_ENTRY.addEventListener('click', function() {
+      closeMenu();
+    });
+  }
 
-	/**
+  /**
    * @description Scrolls to page top when back to top button clicked
-	 *
-	 */
-	BACK_TO_TOP.addEventListener('click', function() {
-		scroll(0,0);
-	});
+   *
+   */
+  BACK_TO_TOP.addEventListener('click', function() {
+    scroll(0,0);
+  });
 
-	/**
+  /**
    * @description Checks on each scroll whether back to top button should be shown
-	 *
-	 */
-	window.addEventListener('scroll', function() {
-		setBacktotopVisibility();
-	});
+   *
+   */
+  window.addEventListener('scroll', function() {
+    setBacktotopVisibility();
+  });
 
 
-	/**
-	 *
-	 * Initial calls
-	 *
-	 */
+  /**
+   *
+   * Initial calls
+   *
+   */
 
-	setBacktotopVisibility();
+  setBacktotopVisibility();
 
 };
